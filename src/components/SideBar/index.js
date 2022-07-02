9a726cafeb75c3b80bd5f0def3e7465eea9c534f
Yusuf Ismail <yusuf@bambu.life>
2022-07-03 02:54:45 +0800

    ✨ add sidebar current route highlight

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -5,9 +5,13 @@ import {
   FaPlayCircle,
   FaRegChartBar,
 } from 'react-icons/fa';
+import { useLocation, useNavigate } from 'react-router-dom';
 import styles from './SideBar.module.css';
 
 const SideBar = () => {
+  let location = useLocation();
+  let navigate = useNavigate();
+
   return (
     <div className={styles['Side-Bar__container']}>
       <div className={styles['Side-Bar__profile_container']}>
@@ -15,23 +19,58 @@ const SideBar = () => {
         <p>Bob Smith</p>
       </div>
       <div className={styles['Side-Bar__buttons_container']}>
-        <button name='Discover'>
+        <button
+          name='Discover'
+          style={{
+            backgroundColor:
+              location.pathname === '/discover' ? '#ffffff40' : 'transparent',
+          }}
+          onClick={() => navigate('/discover')}
+        >
           <FaHeadphonesAlt />
           <p>Discover</p>
         </button>
-        <button name='Search'>
+        <button
+          name='Search'
+          style={{
+            backgroundColor:
+              location.pathname === '/search' ? '#ffffff40' : 'transparent',
+          }}
+          onClick={() => navigate('/search')}
+        >
           <FaSearch />
           <p>Search</p>
         </button>
-        <button name='Favourites'>
+        <button
+          name='Favourites'
+          style={{
+            backgroundColor:
+              location.pathname === '/favourites' ? '#ffffff40' : 'transparent',
+          }}
+          onClick={() => navigate('/favourites')}
+        >
           <FaHeart />
           <p>Favourites</p>
         </button>
-        <button name='Playlists'>
+        <button
+          name='Playlists'
+          style={{
+            backgroundColor:
+              location.pathname === '/playlists' ? '#ffffff40' : 'transparent',
+          }}
+          onClick={() => navigate('/playlists')}
+        >
           <FaPlayCircle />
           <p>Playlists</p>
         </button>
-        <button name='Charts'>
+        <button
+          name='Charts'
+          style={{
+            backgroundColor:
+              location.pathname === '/charts' ? '#ffffff40' : 'transparent',
+          }}
+          onClick={() => navigate('/charts')}
+        >
           <FaRegChartBar />
           <p>Charts</p>
         </button>
