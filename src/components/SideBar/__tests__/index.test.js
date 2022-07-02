5746d600c39dac71ec90e848c05b67b23720ff6a
Yusuf Ismail <yusuf@bambu.life>
2022-07-03 02:56:27 +0800

    ✅ update sidebar unit test

--- src/components/SideBar/__tests__/index.test.js
+++ src/components/SideBar/__tests__/index.test.js
@@ -1,32 +1,53 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import SideBar from '../index';
 
 it('sidebar displays discover button correctly', () => {
-  render(<SideBar />);
+  render(
+    <MemoryRouter>
+      <SideBar />
+    </MemoryRouter>
+  );
   let discoverButton = screen.getByRole('button', { name: 'Discover' });
   expect(discoverButton).toBeInTheDocument();
 });
 
 it('sidebar displays search button correctly', () => {
-  render(<SideBar />);
+  render(
+    <MemoryRouter>
+      <SideBar />
+    </MemoryRouter>
+  );
   let searchButton = screen.getByRole('button', { name: 'Search' });
   expect(searchButton).toBeInTheDocument();
 });
 
 it('sidebar displays favourites button correctly', () => {
-  render(<SideBar />);
+  render(
+    <MemoryRouter>
+      <SideBar />
+    </MemoryRouter>
+  );
   let favButton = screen.getByRole('button', { name: 'Favourites' });
   expect(favButton).toBeInTheDocument();
 });
 
 it('sidebar displays playlists button correctly', () => {
-  render(<SideBar />);
+  render(
+    <MemoryRouter>
+      <SideBar />
+    </MemoryRouter>
+  );
   let playlistButton = screen.getByRole('button', { name: 'Playlists' });
   expect(playlistButton).toBeInTheDocument();
 });
 
 it('sidebar displays charts button correctly', () => {
-  render(<SideBar />);
+  render(
+    <MemoryRouter>
+      <SideBar />
+    </MemoryRouter>
+  );
   let chartButton = screen.getByRole('button', { name: 'Charts' });
   expect(chartButton).toBeInTheDocument();
 });
